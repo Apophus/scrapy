3bb8bec20c40cd7984dac767abaf3c4243e495e7
Apophus <bkilel12@gmail.com>
2016-07-10 22:46:22 +0300

    added function to extract data from the site

--- stack/stack/spiders/stack_spider.py
+++ stack/stack/spiders/stack_spider.py
@@ -1,8 +1,24 @@
 from scrapy import Spider
-
+from scrapy.selector import Selector
 
 class StackSpider(Spider):
+    #name of the spider
     name = 'stack'
+    #base url for spider to crawl
     allowed_domains = ["stackoverflow.com"]
-    start_urls = ["http://stackoverflow.com/questions?pages12e-50&sort=newest",]
+    #list of urls for spider to start crawling from
+    start_urls = ["http://stackoverflow.com/questions?pages12e-50&sort=newest",
+                  ]
+    def parse(self, response):
+        questions = Selector(response).xpath('//div[@class="summary"]/h3')
+
+        #Extract the data
+        for question in questions:
+            item = StackItem()
+            item['title'] = question.xpath(
+                'a[@class="question-hyperlink"]/text()').extract()[0]
+            item['title'] = question.xpath(
+                'a[@class="question-hyperlink"]/@href').extract()[0]
+            yield item
+
 
