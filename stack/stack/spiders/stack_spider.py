973c79bd7ee5a9a0b762cb167732f6f0f87ddddd
Apophus <bkilel12@gmail.com>
2016-07-18 11:31:40 +0300

    set up the url to crawl

--- stack/stack/spiders/stack_spider.py
+++ stack/stack/spiders/stack_spider.py
@@ -1,18 +1,21 @@
 from scrapy import Spider
 from scrapy.selector import Selector
+from stack.stack.items import StackItem
+
 
 class StackSpider(Spider):
-    #name of the spider
+    # name of the spider
     name = 'stack'
-    #base url for spider to crawl
+    # base url for spider to crawl
     allowed_domains = ["stackoverflow.com"]
-    #list of urls for spider to start crawling from
+    # list of urls for spider to start crawling from
     start_urls = ["http://stackoverflow.com/questions?pages12e-50&sort=newest",
                   ]
+
     def parse(self, response):
         questions = Selector(response).xpath('//div[@class="summary"]/h3')
 
-        #Extract the data
+        # Extract the data
         for question in questions:
             item = StackItem()
             item['title'] = question.xpath(
@@ -20,5 +23,3 @@ class StackSpider(Spider):
             item['title'] = question.xpath(
                 'a[@class="question-hyperlink"]/@href').extract()[0]
             yield item
-
-
